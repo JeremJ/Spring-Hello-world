c950fc1c1c4e91d3291452c3aefc17e1f4d6b645
JeremJ <jeremiasz.jaworski@gmail.com>
2019-04-16 13:52:03 +0200

    Initial commit

---
+++ src/main/java/angular/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package angular.helloworld;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @RequestMapping("/")
+    public String hello(){
+        return "Hello World";
+    }
+
+}
